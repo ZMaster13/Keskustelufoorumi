294881ab1f2f3445c91e6a94f0e9b4a5b8b76c54
ZMaster13 <z1master@hotmail.com>
2016-03-31 23:47:14 +0300

    SQL-statements

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,52 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Member
+	(name, password, hash)
+	VALUES ('Käyttäjä', 'Käyttäjä', 'asdf');
+
+
+INSERT INTO Team
+	(name, admin)
+	VALUES ('Käyttäjät', false);
+
+INSERT INTO MemberTeam
+	(member, team)
+	VALUES (1, 1);
+
+INSERT INTO Category
+	(name)
+	VALUES ('Testikategoria');
+
+INSERT INTO Category
+	(name)
+	VALUES ('Testikategoria 2');
+
+INSERT INTO Area
+	(category, name)
+	VALUES (1, 'Testialue');
+
+INSERT INTO Area
+	(category, name)
+	VALUES (1, 'Testialue 2');
+
+INSERT INTO Topic
+	(area, member, name)
+	VALUES (1, 1, 'Testiaihe');
+
+INSERT INTO Topic
+	(area, member, name)
+	VALUES (1, 1, 'Testiaihe 2');
+
+INSERT INTO Topic
+	(area, member, name)
+	VALUES (1, 1, 'Testiaihe 3');
+
+INSERT INTO Message
+	(topic, member, title, message, time)
+	VALUES (1, 1, 'Testiviesti', 'Lorem ipsum dolor sit amet', to_timestamp('2008-10-19 10:54:45', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO Message
+	(topic, member, title, message, time)
+	VALUES (1, 1, 'Testiviesti 2', 'Lorem ipsum dolor sit amet', to_timestamp('2008-10-19 15:54:45', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO Message
+	(topic, member, title, message, time)
+	VALUES (2, 1, 'Testiviesti 3', 'Lorem ipsum dolor sit amet', to_timestamp('2008-15-19 15:22:45', 'YYYY-MM-DD HH24:MI:SS'));
